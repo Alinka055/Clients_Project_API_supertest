8e7a6eb5d794751858ae92ef0e755b6940354ab0
alina <alisita05@gmail.com>
2023-04-25 17:57:06 -0400

    add hooks in auth specs

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,95 +1,97 @@
-import {expect} from "chai"
+import { expect } from 'chai'
 import request from 'supertest'
 import 'dotenv/config'
 
 describe('Authorization test', () => {
-    describe('Authorization with valid credentials', () => {
-//console.log(response.body )
-        it('Response status code is 200', async () => {
-            let response = await request(process.env.BASE_URL)
-                .post('/v5/user/login')
-                .send({email: process.env.EMAIL, password: process.env.PASSWORD})
-            expect(response.statusCode).to.eq(200)
-        })
-        it('Response body returns correct message', async () => {
-            let response = await request(process.env.BASE_URL)
-                .post('/v5/user/login')
-                .send({email: process.env.EMAIL, password: process.env.PASSWORD})
-            expect(response.body.message).to.eq('Auth success')
-        })
-        it('Response body returns token', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-                .send({email: 'forest@owner.com', password: '123123'})
-            expect(response.body.payload.token).to.be.a('string')
-        })
+  describe('Authorization with valid credentials', () => {
+      let response
+      before(async () => {
+          response = await request(process.env.BASE_URL)
+              .post('/v5/user/login')
+              .send({ email: process.env.EMAIL, password: process.env.PASSWORD })
+      })
+    it('Response status code is 200', () => {
+      expect(response.statusCode).to.eq(200)
     })
-    describe('Authorization with invalid email', () => {
-        it('Response body returns code is 400', async () => {
-            let response = await request(process.env.BASE_URL)
-                .post('/v5/user/login')
-                .send({email: 'forest@bear.com', password: 123123})
-            expect(response.statusCode).to.eq(400)
-        })
-        it('Response body returns error message', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-                .send({email: 'forest@bear.com', password: '123123'})
-            expect(response.body.message).to.eq('Auth failed')
-        })
+    it('Response body returns correct message',  () => {
+      expect(response.body.message).to.eq('Auth success')
     })
-    describe('Authorization with invalid password', () => {
-        it('Response body returns code is 400', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-                .send({email: 'forest@owner.com', password: '5555'})
-            expect(response.statusCode).to.eq(400)
-        })
-        it('Response body returns error message', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-                .send({email: 'forest@owner.com', password: '5555'})
-            expect(response.body.message).to.eq('Auth failed')
-        })
+    it('Response body returns token', () => {
+      expect(response.body.payload.token).to.be.a('string')
     })
-    describe('Authorization without email', () => {
-        it('Response body returns code is 400', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-                .send({password: '123123'})
-            expect(response.statusCode).to.eq(400)
-        })
-        it('Response body returns error message', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-                .send({password: '123123'})
-            expect(response.body.message).to.eq('Auth failed')
-        })
+  })
+
+  describe('Authorization with invalid email', () => {
+    let response
+    before(async () => {
+      response = await request(process.env.BASE_URL)
+          .post('/v5/user/login')
+          .send({ email: 'forest@bear.com', password: 123123 })
     })
-    describe('Authorization without password', () => {
-        it('Response body returns code is 400', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-                .send({email: 'forest@owner.com'})
-            expect(response.statusCode).to.eq(400)
-        })
-        it('Response body returns error message', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-                .send({email: 'forest@owner.com'})
-            expect(response.body.message).to.eq('Auth failed')
-        })
+    it('Response body returns code is 400', () => {
+      expect(response.statusCode).to.eq(400)
     })
-    describe('Authorization without any credentials', () => {
-        it('Response body returns code is 400', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-            expect(response.statusCode).to.eq(400)
-        })
-        it('Response body returns error message', async () => {
-            let response = await request('https://clientbase-server.herokuapp.com')
-                .post('/v5/user/login')
-            expect(response.body.message).to.eq('Auth failed')
-        })
+    it('Response body returns error message',() => {
+      expect(response.body.message).to.eq('Auth failed')
     })
+  })
+
+  describe('Authorization with invalid password', () => {
+    let response
+    before(async () => {
+      response = await request('https://clientbase-server.herokuapp.com')
+          .post('/v5/user/login')
+          .send({ email: 'forest@owner.com', password: '5555' })
+    })
+    it('Response body returns code is 400',() => {
+      expect(response.statusCode).to.eq(400)
+    })
+    it('Response body returns error message',() => {
+      expect(response.body.message).to.eq('Auth failed')
+    })
+  })
+
+  describe('Authorization without email', () => {
+    let response
+    before(async () => {
+      response = await request('https://clientbase-server.herokuapp.com')
+          .post('/v5/user/login')
+          .send({ password: '123123' })
+    })
+    it('Response body returns code is 400',() => {
+      expect(response.statusCode).to.eq(400)
+    })
+    it('Response body returns error message', async () => {
+      expect(response.body.message).to.eq('Auth failed')
+    })
+  })
+
+  describe('Authorization without password', () => {
+    let response
+    before(async () => {
+      response = await request('https://clientbase-server.herokuapp.com')
+          .post('/v5/user/login')
+          .send({ email: 'forest@owner.com' })
+    })
+    it('Response body returns code is 400', async () => {
+      expect(response.statusCode).to.eq(400)
+    })
+    it('Response body returns error message', async () => {
+      expect(response.body.message).to.eq('Auth failed')
+    })
+  })
+
+  describe('Authorization without any credentials', () => {
+    let response
+    before(async () => {
+      response = await request('https://clientbase-server.herokuapp.com')
+          .post('/v5/user/login')
+    })
+    it('Response body returns code is 400', async () => {
+      expect(response.statusCode).to.eq(400)
+    })
+    it('Response body returns error message', async () => {
+      expect(response.body.message).to.eq('Auth failed')
+    })
+  })
 })
