1022d698ff53157e8da198ce548807b145068165
alina <alisita05@gmail.com>
2023-04-28 23:45:35 -0400

    add general helper

---
+++ helpers/general.js
@@ -0,0 +1,8 @@
+import request from "supertest";
+
+function login (email, password){
+    return request(process.env.BASE_URL)
+        .post('/v5/user/login')
+        .send({ email, password })
+}
+export {login}
